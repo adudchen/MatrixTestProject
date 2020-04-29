71ea328ff55b0f5923f2b5298bcd36b972729768
adudchen <da394372@gmail.com>
2020-04-29 22:14:02 +0300

    initial

---
+++ src/main/java/Matrix.java
@@ -0,0 +1,104 @@
+public class Matrix {
+
+    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix) {
+        if (firstMatrix[0].length != secondMatrix.length) {
+            throw new IllegalArgumentException("Number of columns of firstMatrix and number of rows of secondMatrix are not equal");
+        }
+
+        double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = multiplyMatricesCell(firstMatrix, secondMatrix, row, col);
+            }
+        }
+        return result;
+    }
+
+    private static double multiplyMatricesCell(double[][] firstMatrix, double[][] secondMatrix, int row, int col) {
+        double cell = 0;
+        for (int i = 0; i < secondMatrix.length; i++) {
+            cell += firstMatrix[row][i] * secondMatrix[i][col];
+        }
+        return cell;
+    }
+
+    public static void print(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    public static double[][] sum(double[][] firstMatrix, double[][] secondMatrix) {
+        if (firstMatrix.length != secondMatrix.length) {
+            throw new IllegalArgumentException("Matrix is not same size");
+        }
+
+        for (int i = 0; i < firstMatrix.length; i++) {
+            if (firstMatrix[i].length != secondMatrix[i].length)
+                throw new IllegalArgumentException("Matrix is not same size");
+        }
+
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] + secondMatrix[row][col];
+            }
+        }
+        return result;
+    }
+
+    public static double[][] diff(double[][] firstMatrix, double[][] secondMatrix) {
+        if (firstMatrix.length != secondMatrix.length) {
+            throw new IllegalArgumentException("Matrix is not same size");
+        }
+
+        for (int i = 0; i < firstMatrix.length; i++) {
+            if (firstMatrix[i].length != secondMatrix[i].length)
+                throw new IllegalArgumentException("Matrix is not same size");
+        }
+
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] - secondMatrix[row][col];
+            }
+        }
+        return result;
+    }
+
+    public static double[][] multiplyOnNum(double[][] matrix, double num) {
+        if (matrix.length == 0)
+            throw new IllegalArgumentException("Matrix is empty");
+
+        double[][] result = new double[matrix.length][matrix[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = matrix[row][col] * num;
+            }
+        }
+        return result;
+    }
+
+    public static double[][] randomMatrix(int rows, int columns) {
+        if (rows <= 0 || columns <= 0) {
+            throw new IllegalArgumentException("Matrix sizes are negative");
+        }
+
+        double [][] result = new double[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = Math.random() * 10;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/matrixTest.java
@@ -0,0 +1,134 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class matrixTest
+{
+    @Test
+    public void multiplyOnNumTest1()
+    {
+        double[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] expectedMat = {{5, 10, 15}, {20, 25, 30}, {35, 40, 45}};
+        Assert.assertArrayEquals(Matrix.multiplyOnNum(matrix, 5), expectedMat);
+    }
+
+    @Test
+    public void multiplyOnNumTest2()
+    {
+        double[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] expectedMat = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        Assert.assertArrayEquals(Matrix.multiplyOnNum(matrix, 0), expectedMat);
+    }
+
+    @Test
+    public void multiplyOnNumTest3()
+    {
+        double[][] matrix = {};
+        assertThrows(IllegalArgumentException.class, () -> Matrix.multiplyOnNum(matrix, 4));
+    }
+
+    @Test
+    public void matrixDiffTest1()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] expectedMatrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        Assert.assertArrayEquals(expectedMatrix, Matrix.diff(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixDiffTest2()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        double[][] expectedMatrix = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        Assert.assertArrayEquals(expectedMatrix, Matrix.diff(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixDiffTest3()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
+        assertThrows(IllegalArgumentException.class, () -> Matrix.diff(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void randMatrixTest1()
+    {
+        double[][] firstMatrix = Matrix.randomMatrix(5, 5);
+        Assert.assertEquals(firstMatrix.length, 5);
+        Assert.assertEquals(firstMatrix[0].length, 5);
+    }
+
+    @Test
+    public void randMatrixTest2()
+    {
+        assertThrows(IllegalArgumentException.class, () -> Matrix.randomMatrix(0, 5));
+    }
+
+    @Test
+    public void randMatrixTest3()
+    {
+        assertThrows(IllegalArgumentException.class, () -> Matrix.randomMatrix(5, -1));
+    }
+
+    @Test
+    public void matrixSumTest1()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] expectedMatrix = Matrix.multiplyOnNum(firstMatrix, 2);
+        Assert.assertArrayEquals(expectedMatrix, Matrix.sum(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixSumTest2()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
+        assertThrows(IllegalArgumentException.class, () -> Matrix.sum(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixMultiplyTest1()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        double[][] expectedMatrix = {{24, 30, 36}, {51, 66, 81}, {78, 102, 126}};
+        Assert.assertArrayEquals(expectedMatrix, Matrix.multiply(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixMultiplyTest2()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0}, {1}, {2}};
+        double[][] expectedMatrix = {{8}, {17}, {26}};
+        Assert.assertArrayEquals(expectedMatrix, Matrix.multiply(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixMultiplyTest3()
+    {
+        double[][] firstMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        double[][] secondMatrix = {{0, 1, 2}};
+        assertThrows(IllegalArgumentException.class, () -> Matrix.multiply(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixMultiplyTest4()
+    {
+        double[][] firstMatrix = Matrix.randomMatrix(100, 100);
+        double[][] secondMatrix = Matrix.randomMatrix(2, 100);
+        assertThrows(IllegalArgumentException.class, () -> Matrix.multiply(firstMatrix, secondMatrix));
+    }
+
+    @Test
+    public void matrixMultiplyTest5()
+    {
+        double[][] firstMatrix = Matrix.randomMatrix(30, 100);
+        double[][] secondMatrix = Matrix.randomMatrix(100, 50);
+        Matrix.multiply(firstMatrix, secondMatrix);
+    }
+}
